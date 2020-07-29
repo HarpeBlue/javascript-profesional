62d8f2f90dad2b33cb707825eff21ed42a2a7438
HarpeBlue <hspe2507@gmail.com>
2020-07-29 10:26:18 -0500

    Service Worker

--- website/assets/index.js
+++ website/assets/index.js
@@ -12,3 +12,7 @@ const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPa
 playButton.onclick = () => player.togglePlay();
 playButtonOn.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => console.error(error))
+}

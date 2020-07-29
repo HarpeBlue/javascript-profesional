a99aa4a6a7557a1ced459b4226ee8f622a04287a
HarpeBlue <hspe2507@gmail.com>
2020-07-29 11:49:05 -0500

    Se estudió el tipado de variables en typescript

--- website/Ejercicios/typescript/index.ts
+++ website/Ejercicios/typescript/index.ts
@@ -1,7 +1,42 @@
-console.log('Hello, TypeScript');
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number,b: number) {
-  return a + b;
+// Numeros
+let age = 6;
+let numerador: number = 42;
+let denominador: number = age;
+let resultado = numerador / denominador;
+
+//string
+let nombre: string = "Harold";
+let saludo = `Hola, me llamo ${nombre}`;
+
+//Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push("9000");
+
+let peopleAndNumbers: Array< string | number > = []
+peopleAndNumbers.push("Harold")
+peopleAndNumbers.push(9001)
+
+// Enum
+
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+  Amarillo = "Amarillo",
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Rojo;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard"};
+
+let someObject: object = {
+  type: "Wildcard",
+}
